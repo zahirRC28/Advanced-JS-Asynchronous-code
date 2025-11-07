e9c3d452a86f5c49d0c2c9e09861475116ee89d4
zahirRC28 <zahirvalentino2812@gmail.com>
2025-11-07 11:58:34 +0100

    mas ejercicios resueltos

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const connect = async (urlAp) => {
 
 //EJERCICIOs PARTE 1
 const urlBreed = `https://dog.ceo/api/breed`;
-const urlUser = `https://api.github.com/users/`;
+const urlUser = `https://api.github.com/users`;
 
 //1.- Declara una funcion getAllBreeds que devuelva un array de strings con todas las razas de perro.
 const getAllBreeds = async () =>{
@@ -75,26 +75,62 @@ const getAllImagesByBreed2 = async (breed) =>{
 };
 
 //EJERCICIOs PARTE 2
-
 // 5.- Declarara una función getGitHubUserProfile(username) que obtenga el perfil de usuario de github a partir de su nombre de usuario. (https://api.github.com/users/{username}).
-let username = `alenriquez96`;
+//let username = `alenriquez96`;
 const getGitHubUserProfile = async (username) =>{
     try {
-        const datos = await connect(`${urlUser}${username}`);
-        console.log(datos);
-        const name = datos.name;
-        console.log(name);
-        return name;
+        const userProfile = await connect(`${urlUser}/${username}`);
+        //console.log(datos);
+        return userProfile;
     } catch (error) {
         console.log(error)
     }
 }
-
+//6.- Declara una función printGithubUserProfile(username) que reciba como argumento el nombre de un usuario (username), retorne {img, name} y pinte la foto y el nombre en el DOM.
 
 const printGithubUserProfile= async (username) =>{
-    
+    try {
+        const datos = await connect(`${urlUser}/${username}`);
+        //console.log(datos);
+        const name = datos.name;
+        const img = datos.avatar_url;
+        //console.log(name);
+        //console.log(img);
+        return {name, img};
+    } catch (error) {
+        console.log(error)
+    }
 }
-
+// 7. Crea una función getAndPrintGitHubUserProfile(username) que contenga una petición a la API para obtener información de ese usuario y devuelva un string que represente una tarjeta HTML como en el ejemplo, la estructura debe ser exactamente la misma:
 const getAndPrintGitHubUserProfile = async (username) =>{
-    
+    const datos = await connect(`${urlUser}/${username}`);
+    //console.log(datos);
+    const name = datos.name;
+    const img = datos.avatar_url;
+    const repos = datos.public_repos;
+    //console.log(repos);
+    const pintar = `
+        <section>
+            <img src="${img}" alt="${name}">
+            <h1>${name}</h1>
+            <p>Public repos: ${repos}</p>
+        </section>`
+    return pintar;
+}
+//9.- Dada una lista de usuarios de github guardada en una array,crea una funcion fetchGithubUsers(userNames) que utilice 'https://api.github.com/users/${name}' para obtener el nombre de cada usuario.
+
+const fetchGithubUsers = async (userNames) =>{
+    try {
+        const urls = "";
+        const data = await urls.map (userNames => connect(`${urlUser}/${userNames}`));
+        console.log(data);
+        Promise.all(data)
+            .then((results) => {
+                console.log(results);
+            }).catch((errores) => {
+                console.log(errores)
+            });
+    } catch (error) {
+        console.log(error);
+    }
 }
